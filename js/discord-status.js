ae3d8539a037d5f37aebf20bc2469945efe16ae0
Ado <76093244+AdoQeT@users.noreply.github.com>
2024-02-02 11:49:11 +0300

    Update discord-status.js

--- js/discord-status.js
+++ js/discord-status.js
@@ -40,21 +40,28 @@ function fetchData() {
                 const activityListElement = document.getElementById('activityList');
                 activityListElement.innerHTML = ''; // Önceki aktiviteleri temizle
 
+                // ...
+
                 activities.forEach(activity => {
                     const activityDiv = document.createElement('div');
                     activityDiv.classList.add('activity');
                 
                     const activityImage = document.createElement('img');
                 
-                    // Spotify aktivitesi ise
-                    if (activity.type === 2 && activity.assets && activity.assets.large_image && activity.assets.large_image.startsWith('spotify:')) {
+                    if (activity.name === 'YouTube') {
+                        // PreMid aktivitesi ise
+                        const largeImage = 'https://media.discordapp.net/attachments/756651694187610195/1202893187119841290/yt.png?ex=65cf1c94&is=65bca794&hm=a5a8c43f6a6bedec75c2926e34c54266105c2d9689f80e143531df25948e7932&=&format=webp&quality=lossless&width=183&height=183'
+                
+                        activityImage.src = largeImage;
+                    } else if (activity.type === 2 && activity.assets && activity.assets.large_image && activity.assets.large_image.startsWith('spotify:')) {
+                        // Spotify aktivitesi ise
                         const spotifyImageId = activity.assets.large_image.split(':')[1];
                         activityImage.src = `https://i.scdn.co/image/${spotifyImageId}`;
                     } else {
                         // Diğer aktiviteler
-                        const appAssets = activity.assets && activity.assets.large_image ? `https://cdn.discordapp.com/app-assets/${activity.application_id}/${activity.assets.large_image}.png` : null;
-                
-                        activityImage.src = appAssets || 'https://cdn.glitch.global/3319ca89-a9f9-4517-bdaa-0c4a50a8df48/unknown_game_img.png?v=1706792626199';
+                        activityImage.src = activity.assets && activity.assets.large_image
+                            ? `https://cdn.discordapp.com/app-assets/${activity.application_id}/${activity.assets.large_image}.png`
+                            : 'https://cdn.glitch.global/3319ca89-a9f9-4517-bdaa-0c4a50a8df48/unknown_game_img.png?v=1706792626199';
                     }
                 
                     activityImage.alt = 'Activity Image';
@@ -81,6 +88,9 @@ function fetchData() {
                 
                     activityListElement.appendChild(activityDiv);
                 });
+
+                // ...
+
             } else {
                 console.error('Lanyard API\'sinden veri çekme başarısız.');
             }
